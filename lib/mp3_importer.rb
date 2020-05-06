33121a366e698da5b0a2037d5fe6d32b8edb8da9
Malcom Fetus <sora.vista@gmail.com>
2020-05-06 22:34:06 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,7 +9,7 @@ def files
    file_array =  Dir.glob("#{path}/*.mp3")
   file_array.collect do |file| #collect returns array
     file.slice!("#{path}/") #slice takes the #{path} as an argument
-    file #use the iteration file from the collect condition on line 10.
+    file #use the iteration file from the collect condition on line 10 to return the updated data
   end
 end
 
